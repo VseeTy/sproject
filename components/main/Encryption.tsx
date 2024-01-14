3691aa96642134e520bcaca88511e5476e4f70d8
VseeTy <mattatoxx@gmail.com>
2024-01-14 12:28:26 +0100

    v4

---
+++ components/main/Encryption.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+const Encryption = () => {
+  return (
+    <div>Encryption</div>
+  )
+}
+
+export default Encryption
--- components/main/Narbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Narbar = () => {
-  return (
-    <div>Narbar</div>
-  )
-}
-
-export default Narbar
